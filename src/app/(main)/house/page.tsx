a299b6ed5ee2c2a1f1a726bdfc100be616179e5c
kiyo-8jo <csta13054@gmail.com>
2025-05-05 20:10:29 +0900

    [update,fix] モックデータをもとにハウス用画面の各部屋情報を表示させる
    モックデータから情報を引っ張り、各部屋のカードに情報を表示させ、エラーを解消しました

--- src/app/(main)/house/page.tsx
+++ src/app/(main)/house/page.tsx
@@ -1,37 +1,12 @@
 import RoomCard from "@/app/components/common/roomCard/RoomCard";
+import { testRooms } from "../../../../testRooms";
 
 const HousePage = () => {
   return (
     <div className="bg-blue-50 flex flex-wrap justify-center h-max mx-5 mt-1 mb-3 p-3 rounded-2xl">
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
-      <RoomCard/>
+      {testRooms.map((room) => (
+        <RoomCard room={room} key={room.id}/>
+      ))}
     </div>
   );
 };
