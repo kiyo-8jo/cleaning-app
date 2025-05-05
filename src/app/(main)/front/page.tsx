2a80d2db19c0ba50538ae712926061d10007ea6f
kiyo-8jo <csta13054@gmail.com>
2025-05-05 19:12:56 +0900

    [update] モックデータをもとにフロント用画面の各部屋情報を表示させる
    モックデータから情報を引っ張り、各部屋のカードに情報を表示させました

--- src/app/(main)/front/page.tsx
+++ src/app/(main)/front/page.tsx
@@ -1,30 +1,12 @@
 import RoomCard from "@/app/components/common/roomCard/RoomCard";
+import { testRooms } from "../../../../testRooms";
 
 const FrontPage = () => {
   return (
     <div className="bg-blue-50 flex flex-wrap justify-center h-full w-[75%] ml-5 mb-3 p-3 rounded-2xl">
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
+      {testRooms.map((room) => (
+        <RoomCard room={room} key={room.id}/>
+      ))}
     </div>
   );
 };
--- src/app/components/common/roomCard/RoomCard.tsx
+++ src/app/components/common/roomCard/RoomCard.tsx
@@ -1,29 +1,36 @@
-const RoomCard = () => {
+import { RoomType } from "@/app/types/types";
+
+interface RoomCardType {
+  room:RoomType
+}
+
+
+const RoomCard = ({room}:RoomCardType) => {
   return (
     <div className="bg-blue-200 flex flex-col w-[225px] h-[160px] rounded-2xl p-2 cursor-pointer m-1">
       <div className="flex items-center justify-between h-[15%] w-full px-1 mb-1 font-bold">
-        <div>221(OTP)</div>
-        <div>IN</div>
+        <div>{room.id}({room.roomType})</div>
+        <div>{room.cleaningType}</div>
       </div>
       <div className="flex items-center justify-around h-[25%] w-full border-t-1 border-b-1 border-gray-500">
-        <div>Icon</div>
-        <div>Icon</div>
+        <div>{room.isKeyBack.toString()}</div>
+        <div>{room.isCleaningComplete.toString()}</div>
       </div>
       <div className="flex h-[60%] w-full mt-1 text-sm">
         <div className="flex flex-col w-1/2 gap-1">
           <div className="flex flex-col w-full items-center">
             <p className="font-bold">Beds</p>
-            <p>3→3</p>
+            <p>{room.nowBeds}→{room.newBeds}</p>
           </div>
           <div className="flex flex-col w-full items-center">
             <p className="font-bold">Guests</p>
-            <p>3/0/0</p>
+            <p>{room.adult}/{room.inf}/{room.kidInf}</p>
           </div>
         </div>
         <div className="flex flex-col w-1/2 items-center">
           <p className="font-bold">Memo</p>
           <p className="w-full h-ful wrap-break-word overflow-y-scroll hidden-scrollbar select-none text-xs p-1 text-center">
-            加湿器入れる
+            {room.memo}
           </p>
         </div>
       </div>
