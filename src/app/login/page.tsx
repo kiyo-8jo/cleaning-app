51e9ad2d894668d1829f4ee42006628f1533003d
kiyo-8jo <csta13054@gmail.com>
2025-05-05 13:55:30 +0900

    [add] ログイン機能の実装
    Supabaseの認証機能を実装するためにnpmのインストールとミドルウェアを作成しました

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,16 @@
+import { signInWithGoogle } from "./actions";
+
 const LoginPage = () => {
   return (
     <div className="bg-amber-50 flex items-center justify-center w-full h-[95vh]">
       <form className="bg-gray-300 flex flex-col items-center justify-center w-[300px] h-[200px] gap-10 rounded-2xl">
         <p className="text-lg">Googleアカウントでログインする</p>
-        <button className="bg-blue-600 px-5 py-2 rounded-3xl text-white text-lg">Log In</button>
+        <button
+          className="bg-blue-600 px-5 py-2 rounded-3xl text-white text-lg"
+          formAction={signInWithGoogle}
+        >
+          Log In
+        </button>
       </form>
     </div>
   );
