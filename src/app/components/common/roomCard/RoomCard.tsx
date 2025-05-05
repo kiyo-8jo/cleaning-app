75165951fabfd6209338a05ad9dee46c7a632185
kiyo-8jo <csta13054@gmail.com>
2025-05-05 19:37:08 +0900

    [update] フロント用画面で部屋カードの背景色を動的に変更する機能実装
    部屋カードの背景色を、部屋の清掃タイプによって動的に変更できる機能を作成しました

--- src/app/components/common/roomCard/RoomCard.tsx
+++ src/app/components/common/roomCard/RoomCard.tsx
@@ -1,15 +1,32 @@
 import { RoomType } from "@/app/types/types";
 
 interface RoomCardType {
-  room:RoomType
+  room: RoomType;
 }
 
-
-const RoomCard = ({room}:RoomCardType) => {
+const RoomCard = ({ room }: RoomCardType) => {
+  // カードの背景色を取得
+  const getBgColor = (cleaningType: string, stayCleaningType: string) => {
+    if (cleaningType === "IN") return "bg-blue-200";
+    if (cleaningType === "OUT") return "bg-red-200";
+    if (cleaningType === "OUT-IN") return "bg-purple-300";
+    if (cleaningType === "NONE") return "bg-gray-400";
+    if (cleaningType === "STAY" && stayCleaningType === "NORMAL")
+      return "bg-yellow-200";
+    if (cleaningType === "STAY" && stayCleaningType === "ECO")
+      return "bg-green-200";
+  };
   return (
-    <div className="bg-blue-200 flex flex-col w-[225px] h-[160px] rounded-2xl p-2 cursor-pointer m-1">
+    <div
+      className={`${getBgColor(
+        room.cleaningType,
+        room.stayCleaningType
+      )}  flex flex-col w-[225px] h-[160px] rounded-2xl p-2 cursor-pointer m-1`}
+    >
       <div className="flex items-center justify-between h-[15%] w-full px-1 mb-1 font-bold">
-        <div>{room.id}({room.roomType})</div>
+        <div>
+          {room.id}({room.roomType})
+        </div>
         <div>{room.cleaningType}</div>
       </div>
       <div className="flex items-center justify-around h-[25%] w-full border-t-1 border-b-1 border-gray-500">
@@ -20,11 +37,15 @@ const RoomCard = ({room}:RoomCardType) => {
         <div className="flex flex-col w-1/2 gap-1">
           <div className="flex flex-col w-full items-center">
             <p className="font-bold">Beds</p>
-            <p>{room.nowBeds}→{room.newBeds}</p>
+            <p>
+              {room.nowBeds}→{room.newBeds}
+            </p>
           </div>
           <div className="flex flex-col w-full items-center">
             <p className="font-bold">Guests</p>
-            <p>{room.adult}/{room.inf}/{room.kidInf}</p>
+            <p>
+              {room.adult}/{room.inf}/{room.kidInf}
+            </p>
           </div>
         </div>
         <div className="flex flex-col w-1/2 items-center">
