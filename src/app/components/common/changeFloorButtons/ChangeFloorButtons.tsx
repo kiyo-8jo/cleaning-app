2fd03ae95c5a7224313adb4c38544cbe0a8c1b72
kiyo-8jo <csta13054@gmail.com>
2025-05-06 19:36:52 +0900

    [update] フロント、ハウス用画面において現在いる階のボタンにcssがあたる機能実装
    1F、2Fのうち現在指定されている階のボタンにボーダーがあたる機能を作成しました

--- src/app/components/common/changeFloorButtons/ChangeFloorButtons.tsx
+++ src/app/components/common/changeFloorButtons/ChangeFloorButtons.tsx
@@ -6,18 +6,22 @@ import { useAppDispatch, useAppSelector } from "@/app/lib/hooks/hooks";
 const ChangeFloorButtons = () => {
   const dispatch = useAppDispatch();
   const { is1f } = useAppSelector((state) => state.is1f);
-  console.log(is1f);
+
   return (
     <div className="flex items-center justify-center gap-50 h-[50px] mt-1">
       <div
         onClick={() => dispatch(setIs1fTrue())}
-        className="bg-blue-200 px-5 py-2 text-center rounded-2xl cursor-pointer font-semibold"
+        className={`${
+          is1f && "border-2"
+        } bg-blue-200 px-5 py-2 text-center rounded-2xl cursor-pointer font-semibold`}
       >
         1F
       </div>
       <div
         onClick={() => dispatch(setIs1fFalse())}
-        className="bg-blue-200 px-5 py-2 text-center rounded-2xl cursor-pointer font-semibold"
+        className={`${
+          !is1f && "border-2"
+        } bg-blue-200 px-5 py-2 text-center rounded-2xl cursor-pointer font-semibold`}
       >
         2F
       </div>
