f37b1b30d14e105eb10cc9c96c01c216563a5ec5
kiyo-8jo <csta13054@gmail.com>
2025-05-10 16:32:34 +0900

    [add] 制作用画面の外観作成
    制作用画面のhtmlとcssを作成しました

--- src/app/(main)/create/page.tsx
+++ src/app/(main)/create/page.tsx
@@ -1,5 +1,30 @@
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  return (
+    <main className="bg-orange-100 h-[95vh] flex flex-col justify-center items-center">
+      <div className="flex gap-30">
+        <div className="bg-green-600 flex flex-col justify-center w-[350px] p-5 rounded-2xl">
+          <label className="flex justify-center mb-5 text-2xl font-medium">
+            1Fのデータを選択
+          </label>
+          <input
+            id="file_input"
+            type="file"
+            className="file:mr-4 file:rounded-full file:bg-gray-50 file:p-3 file:text-sm file:font-semibold cursor-pointer file:cursor-pointer"
+          />
+        </div>
+        <div className="bg-green-600 flex flex-col justify-center w-[350px] p-5 rounded-2xl">
+          <label className="flex justify-center mb-5 text-2xl font-medium">
+            2Fのデータを選択
+          </label>
+          <input
+            type="file"
+            id="2f"
+            className="file:mr-4 file:rounded-full file:bg-gray-50 file:p-3 file:text-sm file:font-semibold cursor-pointer file:cursor-pointer"
+          />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default CreatePage;
